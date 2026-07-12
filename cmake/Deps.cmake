502fecf9f17111724c1347903533befb48e91127
rangeset <2432647223@qq.com>
2026-07-12 14:24:55 +0800

    chore(deps): remove the CLI deps
    remove the CLI11 dependency
    because the GUI will be added
    without in the Terminal

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,13 +1,4 @@
 include(FetchContent)
 
-FetchContent_Declare(
-        CLI11
-        GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-        GIT_TAG v2.6.2
-        GIT_SHALLOW TRUE
-)
-
-FetchContent_MakeAvailable(CLI11)
-
 add_library(ProtocolLabDeps INTERFACE)
-target_link_libraries(ProtocolLabDeps INTERFACE CLI11::CLI11)
+#target_link_libraries(ProtocolLabDeps INTERFACE )
