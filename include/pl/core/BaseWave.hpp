340a5edb65aa9d1a9bc090b4e5e245a537324841
rangeset <2432647223@qq.com>
2026-07-12 20:29:16 +0800

    feat(core): add some function for BaseWave
    add some function for BaseWave to create and calculate

--- include/pl/core/BaseWave.hpp
+++ include/pl/core/BaseWave.hpp
@@ -7,18 +7,35 @@
 // Header file of the base RF wave class.
 
 #pragma once
+
+#include <cmath>
 #include <pl/inter/ISignal.hpp>
+#include <cstdlib>
+
+using namespace std;
+
+class BaseWaveStatus
+{
+    double phase_;
+    double value_sin_{};
+    double value_cos_{};
+    const double omega_sin_;
+    const double omega_cos_;
+public:
+    static BaseWaveStatus buildBaseWaveStatus(double phase, double omega);
+    BaseWaveStatus(double phase, double omega_sin, double omega_cos);
+    void step();
+    void step(size_t step);
+    void calibrate();
+};
 
 class BaseWave : public IGetSignal
 {
     double frequency_;
     double amplitude_;
-    double phase_;
+    BaseWaveStatus status_;
 public:
-    BaseWave(const double frequency, const double amplitude, const double phase)
-        : frequency_(frequency)
-        , amplitude_(amplitude)
-        , phase_(phase) {}
+    BaseWave(double frequency, double amplitude, double phase);
 
     SignalData getValue(double time) override;
 };
--- src/core/BaseWave.cpp
+++ src/core/BaseWave.cpp
@@ -7,3 +7,25 @@
 // Base RF wave class.
 
 #include <pl/core/BaseWave.hpp>
+#include <numbers>
+
+BaseWaveStatus BaseWaveStatus::buildBaseWaveStatus(double phase, double omega)
+{
+    double omega_sin, omega_cos;
+    sincos(omega, &omega_sin, &omega_cos);
+    return {phase, omega_sin, omega_cos};
+}
+
+BaseWaveStatus::BaseWaveStatus
+(const double phase, const double omega_sin, const double omega_cos)
+    : phase_(phase)
+    , omega_sin_(omega_sin)
+    , omega_cos_(omega_cos)
+{
+    sincos(phase, &value_sin_, &value_cos_);
+}
+
+BaseWave::BaseWave(const double frequency, const double amplitude, const double phase)
+    : frequency_(frequency)
+    , amplitude_(amplitude)
+    , status_(BaseWaveStatus::buildBaseWaveStatus(phase, 2 * numbers::pi * frequency)) {}
