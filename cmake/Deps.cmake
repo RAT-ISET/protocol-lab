3c13e842447f630f0073cdfe23a302c998717b34
rangeset <2432647223@qq.com>
2026-07-12 15:52:02 +0800

    chore(deps): add the imgui deps
    add the imgui library to make gui

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,4 +1,13 @@
 include(FetchContent)
 
+FetchContent_Declare(
+        imgui
+        GIT_REPOSITORY https://github.com/ocornut/imgui.git
+        GIT_TAG v1.92.8
+        GIT_SHALLOW TRUE
+)
+
+FetchContent_MakeAvailable(imgui)
+
 add_library(ProtocolLabDeps INTERFACE)
-#target_link_libraries(ProtocolLabDeps INTERFACE )
+target_link_libraries(ProtocolLabDeps INTERFACE imgui)
