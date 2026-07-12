735cb373a53d5b454d2b6e51be044042fbd78c93
rangeset <2432647223@qq.com>
2026-07-12 16:14:53 +0800

    chore(deps): add the glfw deps
    add the glfw library to make gui

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,6 +1,14 @@
 include(FetchContent)
 
 FetchContent_Declare(
+        glfw
+        GIT_REPOSITORY https://github.com/glfw/glfw.git
+        GIT_TAG 3.4
+        GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(glfw)
+
+FetchContent_Declare(
         imgui
         GIT_REPOSITORY https://github.com/ocornut/imgui.git
         GIT_TAG v1.92.8
@@ -8,6 +16,14 @@ FetchContent_Declare(
 )
 
 FetchContent_MakeAvailable(imgui)
+set(imgui_SOURCE_DIR ${CMAKE_BINARY_DIR}/_deps/imgui-src)
+add_library(imgui STATIC
+    ${imgui_SOURCE_DIR}/imgui.cpp
+    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+)
+target_include_directories(imgui PUBLIC ${imgui_SOURCE_DIR})
 
 add_library(ProtocolLabDeps INTERFACE)
-target_link_libraries(ProtocolLabDeps INTERFACE imgui)
+target_link_libraries(ProtocolLabDeps INTERFACE imgui glfw)
