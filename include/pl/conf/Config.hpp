c9c8baca96560c37de4e875a5fd929433fc099bd
rangeset <2432647223@qq.com>
2026-07-19 15:35:02 +0800

    feat(conf): add conf module
    add the module for manager configure

---
+++ include/pl/conf/Config.hpp
@@ -0,0 +1,9 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/protocol-lab
+// ==============================================================
+// Path /include/pl/conf/Config.hpp
+// Header file of the configuration reader and writer.
+
+#pragma once
---
+++ src/conf/Config.cpp
@@ -0,0 +1,9 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/protocol-lab
+// ==============================================================
+// Path /src/conf/Config.cpp
+// Configuration reader and writer.
+
+#include <pl/conf/Config.hpp>
