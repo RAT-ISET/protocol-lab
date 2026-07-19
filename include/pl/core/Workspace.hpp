caeabed0c34ad390bac413aeecc0efb824a4eb89
rangeset <2432647223@qq.com>
2026-07-19 13:10:39 +0800

    feat(core): add workspace
    define the workspace in Workspace.hpp

---
+++ include/pl/core/Workspace.hpp
@@ -0,0 +1,14 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/protocol-lab
+// ==============================================================
+// Path /include/pl/core/Workspace.hpp
+// Header file of the software workspace.
+
+#pragma once
+
+class Workspace
+{
+public:
+};
---
+++ src/core/Workspace.cpp
@@ -0,0 +1,9 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/protocol-lab
+// ==============================================================
+// Path /src/core/Workspace.cpp
+// Software workspace.
+
+#include <pl/core/Workspace.hpp>
